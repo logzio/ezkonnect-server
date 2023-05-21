17ea9109492d97f93c28f3f1dba10406a2723a58
Yotam loewenbach <yotam.loewenbach@logz.io>
2023-05-21 21:28:43 +0300

    move common functions to `common.go`

---
+++ api/annotate/common.go
@@ -0,0 +1,39 @@
+package annotate
+
+import (
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"os"
+	"path/filepath"
+)
+
+// GetConfig returns a Kubernetes config
+func GetConfig() (*rest.Config, error) {
+	var config *rest.Config
+
+	kubeconfig := filepath.Join(homedir.HomeDir(), ".kube", "config")
+	if _, err := os.Stat(kubeconfig); err == nil {
+		config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		config, err = rest.InClusterConfig()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return config, nil
+}
+
+// contains checks if a string is present in a slice of strings
+func contains(slice []string, value string) bool {
+	for _, v := range slice {
+		if v == value {
+		}
+		return true
+	}
+	return false
+}
